071ee0dfb7f740b92895bfc5c3f3d5e69ea3b839
hiu <hoinon2007@gmail.com>
2026-05-28 10:25:55 +0700

    buoi3

---
+++ js/lib.js
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="vi">
+    <head>
+        <meta charset="UTF-8" />
+        <title>thong tin sinh vien</title>
+    </head>
+    <body>
+        <a></a>
+    </body>
+</html>
